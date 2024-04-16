15b72cc163d1526cd48d76635073e6ba1b4433ed
oketafred <oketafred@gmail.com>
2024-04-16 10:43:31 +0300

    WIP

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { StreamBarcodeReader } from "@teckel/vue-barcode-reader";
+</script>
+
+<template>
+  <div>
+    <StreamBarcodeReader torch no-front-cameras @decode="onDecode" @loaded="onLoaded" />
+  </div>
+</template>
